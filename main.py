856888665039662b5c3054506acce0a05f5f934d
Rafael <raphael.yakupov.99@gmail.com>
2021-10-25 02:39:54 +0300

    final fix

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import random
+from datetime import timedelta, date
 
 alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)] + [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-picture = "+**+*+++**+*****++++**+++**+++***+++*+**+***********\
-+*++*+*+**+*****+**+*+****+***+*+****+*++***********\
-++*+*+*+**+++**+++++*+****+***+*+****++*+***********\
-+**+*+**+*+++*+****+**+++**+++***+++*+**+***********\
+picture = "+**+**+++*+*****++++**+++**+++***+++*+**+***********\
++*++**+*+*+*****+**+*+****+***+*+****+*++***********\
+++*+**+*+*+++***++++*+****+***+*+****++*+***********\
++**+*+**+*+++*++***+**+++**+++***+++*+**+***********\
 ****************************************************\
 ****************************************************\
 ****************************************************"
@@ -24,11 +25,19 @@ def add_content(content):
 
 
 def git_commit(digest, date):
-    os.system("git commit -m \"" + digest + "\" --date=\"" + date + "\"")
+    os.system("git commit -m \"" + str(digest) + "\" --date=\"" + str(date) + "T12:00:00+0300\"")
 
 
 def git_add():
     os.system("git add file.txt")
+
+
+def fill_point(delta):
+    message = generate_message()
+    digest = get_digest(message)
+    add_content(message)
+    git_add()
+    git_commit(digest, date.today().replace(2017, 1, 1) + timedelta(days=delta))
 
 
 if __name__ == '__main__':
@@ -36,12 +45,4 @@ if __name__ == '__main__':
         for j in range(7):
             sym = picture[j * 52 + i]
             if sym == "+":
-                print("*", end="")
-            else:
-                print(" ", end="")
-        print()
-    # message = generate_message()
-    # add_content(message)
-    # digest = get_digest(message)
-    # git_add()
-    # git_commit(digest)
+                fill_point(j+i*7)
